010c2912765c268c80c813b033a0e2f5ee9b9c56
ThomasMoerschel <tj.moerschel@gmail.com>
2021-07-23 11:55:44 -0400

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace MemeTown.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Random()
+        {
+            
+            return View();
+        }
     }
 }
